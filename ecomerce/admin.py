66f4f97472f608ad9faa2aeb38bc4ee20b814e79
Diyorbek <diyorbekramonovich02s@gmail.com>
2025-02-01 12:29:13 +0500

    ok

--- ecomerce/admin.py
+++ ecomerce/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+
+from ecomerce.models import Customer, Product
 
 # Register your models here.
+admin.site.register(Customer)
+admin.site.register(Product)
