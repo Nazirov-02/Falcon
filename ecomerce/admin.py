06ef022a090c68588480448c2bedc072d031348c
Diyorbek <diyorbekramonovich02s@gmail.com>
2025-02-04 12:11:14 +0500

    ok3

--- ecomerce/admin.py
+++ ecomerce/admin.py
@@ -1,7 +1,12 @@
+from django.utils.html import format_html
 from django.contrib import admin
-
 # Register your models here.
 from .models import Category, Product, Img
+
+admin.site.site_header = 'apelsin administration'
+admin.site.site_title = 'Manage products'
+admin.site.index_title = 'apelsin shop admin'
+
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
@@ -10,9 +15,23 @@ class CategoryAdmin(admin.ModelAdmin):
 
 class ProductImageInline(admin.TabularInline):  # yoki admin.StackedInline
     model = Img
-    extra = 1  # Yangi rasm qo'shish uchun boshlang'ich qator
+
 
 class ProductAdmin(admin.ModelAdmin):
-    inlines = [ProductImageInline]  # Product bilan bog'langan rasmlarni ko'rsatish
-
+    list_display = ('category','name','price','discount','stock')
+    search_fields = ('name','description')
+    list_filter = ('category','rating')
+    inlines = [ProductImageInline]
 admin.site.register(Product, ProductAdmin)
+
+
+
+
+@admin.register(Img)
+class ImgAdmin(admin.ModelAdmin):
+    list_display = ('product', 'image_preview')
+
+    def image_preview(self, obj):
+        if obj.image:
+            return format_html('<img src="{}" style="height: 100px;" />', obj.image.url)
+        return "No image"
--- ecomerce/urls.py
+++ ecomerce/urls.py
@@ -1,9 +1,10 @@
 
 from django.urls import path
 from ecomerce import views
+
 urlpatterns = [
 path('', views.product_list, name='product_list'),
 path('detail/<int:pk>/',views.product_detail, name='product_detail'),
-    path('customers/',views.customers, name='customers'),
-    path('customer_detail/',views.customer_detail, name='customer_detail'),
+path('customers/',views.customers, name='customers'),
+path('customer_detail/',views.customer_detail, name='customer_detail'),
 ]
